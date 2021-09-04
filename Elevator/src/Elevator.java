17460638818556a57262851c0e852a83a132c99f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-04 23:25:52 +0300

    elevator created

---
+++ Elevator/src/Elevator.java
@@ -0,0 +1,43 @@
+public class Elevator {
+    private int currentFloor = 1;
+    private int minFloor;
+    private int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void moveDown() {
+        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor;
+    }
+
+    public void moveUp() {
+        currentFloor = currentFloor + 1;
+    }
+
+    public void move(int floor) {
+
+        if (floor < minFloor || floor > maxFloor) {
+            System.out.println("Ошибка");
+            return;
+        }
+
+        if (floor > currentFloor) {
+            while (floor > currentFloor) {
+                moveUp();
+                System.out.println("Вы находитесь на " + getCurrentFloor() + " этаже");
+            }
+        }
+        if (floor < currentFloor) {
+            while (floor < currentFloor) {
+                moveDown();
+                System.out.println("Вы находитесь на " + getCurrentFloor() + " этаже");
+            }
+        }
+    }
+}
