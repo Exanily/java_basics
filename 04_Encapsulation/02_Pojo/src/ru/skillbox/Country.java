25c341a24c7d346b4571d1726e4cd5e1df404c05
BuildTools <unconfigured@null.spigotmc.org>
2021-09-05 22:57:07 +0300

    created class "Country"

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Country.java
@@ -0,0 +1,53 @@
+package ru.skillbox;
+
+public class Country {
+    private String nameCountry;
+    private int population;
+    private double square; //km^2
+    private String capitalName;
+    private boolean sea;
+
+    public Country(String nameCountry) {
+        this.nameCountry = nameCountry;
+    }
+
+    public String getNameCountry() {
+        return nameCountry;
+    }
+
+    public void setNameCountry(String nameCountry) {
+        this.nameCountry = nameCountry;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+    public void setCapitalName(String capitalName) {
+        this.capitalName = capitalName;
+    }
+
+    public boolean getSea() {
+        return sea;
+    }
+
+    public void setSea(boolean sea) {
+        this.sea = sea;
+    }
+}
--- 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
@@ -1,7 +1,17 @@
 package ru.skillbox;
 
 public class Main {
-
     public static void main(String[] args) {
+        Country germany = new Country("Германия"); // метод setNameCountry не использован, так как в этой строке уже задано название страны
+        germany.setCapitalName("Берлин");
+        germany.setPopulation(83020000);
+        germany.setSquare(357386);
+        germany.setSea(true);
+
+        System.out.println("Название страны : " + germany.getNameCountry());
+        System.out.println("Численность населения : " + germany.getPopulation());
+        System.out.println("Площадь в квадратных километрах : " + germany.getSquare() + " км^2");
+        System.out.println("Название столицы : " + germany.getCapitalName());
+        System.out.println("Наличие выхода к морю : " + germany.getSea());
     }
 }
