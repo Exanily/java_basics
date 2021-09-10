34ee1d962b1f728411ac4685a6fa172aa5730b28
BuildTools <unconfigured@null.spigotmc.org>
2021-09-10 13:30:58 +0300

    fixed method

--- 05_StaticFinal/02_enum/src/ru/skillbox/ArithmeticCalculator.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/ArithmeticCalculator.java
@@ -8,21 +8,15 @@ public class ArithmeticCalculator {
         this.a = a;
         this.b = b;
     }
-    public void getCalculate(){
-        System.out.println("Сложение : " + calculate(Operation.ADD));
-        System.out.println("Вычитание  : " + calculate(Operation.SUBTRACT));
-        System.out.println("Умножение : " + calculate(Operation.MULTIPLY));
-    }
+
     public int calculate(Operation operation) {
         if (operation == Operation.ADD) {
             return a + b;
-        }
-        if (operation == Operation.SUBTRACT) {
+        } else if (operation == Operation.SUBTRACT) {
             return a - b;
-        }
-        if (operation == Operation.MULTIPLY) {
+        } else if (operation == Operation.MULTIPLY) {
             return a * b;
-        }
-        return 0;
+        } else return 0;
+
     }
 }
--- 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         ArithmeticCalculator calculator = new ArithmeticCalculator(9,6);
-        calculator.getCalculate();
+        System.out.println(calculator.calculate(Operation.ADD));
+        System.out.println(calculator.calculate(Operation.SUBTRACT));
+        System.out.println(calculator.calculate(Operation.MULTIPLY));
+
     }
 }
