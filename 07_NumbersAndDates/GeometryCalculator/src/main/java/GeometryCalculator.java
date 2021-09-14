e78c3345238b222c9e2f940fc5db747ba5c579db
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 00:18:10 +0300

    task solved

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,25 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        return Math.PI * radius * radius;
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        return 4.0 / 3 * Math.PI * Math.abs(radius * radius * radius);
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+        return a + b > c && a + c > b && b + c > a;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        if (isTrianglePossible(a, b, c)) {
+            double p = (a + b + c) / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return -1;
     }
 }
--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,10 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final int MAX_BOX = 27;
+    private static final int MAX_CONTAINER = 12;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int box = (int) Math.ceil(Double.parseDouble(boxes));
+        int container = (int) Math.ceil((double) box / MAX_BOX);
+        int truck = (int) Math.ceil((double) container / MAX_CONTAINER);
 
+        int container_i = 1;
+        int box_i = 1;
+        for (int i = 1; i <= truck; i++) {
+            System.out.println("Грузовик: " + i);
+            for (int j = container_i; j <= container; j++) {
+                System.out.println("\tКонтейнер: " + j);
+                for (int f = box_i; f <= box; f++) {
+                    System.out.println("\t\tЯщик: " + f);
+                    if (f % MAX_BOX == 0) {
+                        box_i = ++f;
+                        break;
+                    }
+                }
+                if (j % MAX_CONTAINER == 0) {
+                    container_i = ++j;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + truck + " шт.");
+        System.out.println("контейнеров - " + container + " шт.");
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
