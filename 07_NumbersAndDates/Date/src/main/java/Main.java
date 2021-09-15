b1c05643e48e790f105acb607d7b17c04599ae3c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 23:17:35 +0300

    task solved

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Main {
+
 
     public static void main(String[] args) {
 
@@ -8,7 +14,6 @@ public class Main {
         int year = 1990;
 
         System.out.println(collectBirthdays(year, month, day));
-
     }
 
     public static String collectBirthdays(int year, int month, int day) {
@@ -16,7 +21,16 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        String text = "";
+
+        for (int i = 0; today.compareTo(birthday) >= 0; i++) {
+            text += i + " - " + formatter.format(birthday) + " - " + birthday.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US) + System.lineSeparator();
+            birthday = birthday.plusYears(1);
+        }
+        return text;
     }
 }
--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -2,12 +2,15 @@ import java.time.LocalDate;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(1995 , 5, 23);
+        System.out.println(getPeriodFromBirthday(date));
+    }
 
-  }
-
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
-  }
+    private static String getPeriodFromBirthday(LocalDate birthday) {
+        LocalDate date = LocalDate.now();
+        LocalDate dat = date.minusYears(birthday.getYear()).minusMonths(birthday.getMonthValue()).minusDays(birthday.getDayOfMonth());
+        return dat.getYear() + " years , " + dat.getMonthValue() + " months , " + dat.getDayOfMonth() + " days" ;
+    }
 
 }
