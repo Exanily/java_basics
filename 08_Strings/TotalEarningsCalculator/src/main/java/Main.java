1c3e5431f9d1e098cdf4219b5732b413efdd1134
BuildTools <unconfigured@null.spigotmc.org>
2021-09-20 19:36:52 +0300

    task solved

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -5,20 +5,19 @@ public class Main {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         //TODO: напишите ваш код, результат вывести в консоль
 
-        String charStr = "заработал";
-        String charStr2 = "рублей";
-        String charStr3 = "-";
-        String charStr4 = "рубля";
-        int start = text.indexOf(charStr) + charStr.length() + 1;
-        int end = text.indexOf(charStr2, start) - 1;
+        String charStr = "5000";
+        String charStr2 = "7563";
+        String charStr3 = "30000";
+        int start = text.indexOf(charStr);
+        int end = text.indexOf(charStr) + 4;
         String text2 = text.substring(start, end);
         int money = Integer.parseInt(text2);
-        start = text.indexOf(charStr3) + charStr3.length() + 1;
-        end = text.indexOf(charStr4, start) - 1;
+        start = text.indexOf(charStr2);
+        end = text.indexOf(charStr2) + 4;
         text2 = text.substring(start, end);
         money += Integer.parseInt(text2);
-        start = text.lastIndexOf(charStr3) + charStr3.length() + 1;
-        end = text.indexOf(charStr2, start) - 1;
+        start = text.indexOf(charStr3);
+        end = text.indexOf(charStr3) + 5;
         text2 = text.substring(start, end);
         money += Integer.parseInt(text2);
         System.out.println(money);
