31a7ffffc4e3d41ca3a096c5e54594ba6236fb08
BuildTools <unconfigured@null.spigotmc.org>
2021-09-22 23:22:05 +0300

    task solved

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,50 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            System.out.println(fullName(input));
+        }
     }
-  }
 
+    public static String fullName(String input) {
+        char ch;
+        int space = 0;
+        for (int i = 0; i < input.length(); i++) {
+            ch = input.charAt(i);
+            if (ch == ' ') {
+                space++;
+            }
+        }
+        if (space != 2 || input.charAt(0) == ' ' || input.charAt(0) == '-') {
+            return "Введенная строка не является ФИО";
+        }
+        String[] name = new String[3];
+        String regex = "[А-я-]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        int i = 0;
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            name[i] = input.substring(start, end);
+            i++;
+        }
+        for (int j = 0; j < 3; j++) {
+            if (name[0] == null) {
+                return "Введенная строка не является ФИО";
+            }
+        }
+
+        return String.format("Фамилия: %s%nИмя: %s%nОтчество: %s", name[0], name[1], name[2]);
+    }
 }
--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -2,16 +2,32 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            System.out.println(phoneNumber(input));
+        }
     }
-  }
 
+    public static StringBuilder phoneNumber(String input) {
+        String regex = "[^0-9]";
+        StringBuilder number;
+        number = new StringBuilder(input.replaceAll(regex, ""));
+
+        if (number.length() == 11) {
+            if (number.charAt(0) == '8' || number.charAt(0) == '7') {
+                if (number.charAt(0) == '8') {
+                    return number.replace(0, 1, "7");
+                }
+                return number;
+            } else return new StringBuilder("Неверный формат номера");
+        } else if (number.length() == 10) {
+            return number.reverse().append("7").reverse();
+        } else return new StringBuilder("Неверный формат номера");
+    }
 }
