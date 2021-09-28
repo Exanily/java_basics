3dfc666baef6dd4c4916976a3e2a46ecc52271ca
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 16:05:16 +0300

    task fixed

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+
     public static EmailList emailList = new EmailList();
+
     public static final Pattern PATTERN = Pattern.compile("[\\w.-]+@[A-z]+\\.[A-z]+");
     public static final Pattern PATTERN_ADD = Pattern.compile("ADD (.)+");
     public static final Pattern PATTERN_LIST = Pattern.compile("LIST");
+
+    private static Matcher matcher;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,18 +26,20 @@ public class Main {
     }
 
     public static void commands(String text) {
-        if (text.matches(PATTERN_ADD.pattern())) {
+        matcher = PATTERN_ADD.matcher(text);
+        if (matcher.matches()) {
             String email = text.substring(text.indexOf(' ') + 1);
             if (checkRegex(email)) {
                 emailList.add(email);
             } else System.out.println(WRONG_EMAIL_ANSWER);
-        } else if (text.matches(PATTERN_LIST.pattern())) {
+        } else if (PATTERN_LIST.matcher(text).matches()) {
             emailList.list();
         } else
             System.out.println("Неверная команда");
     }
 
     public static boolean checkRegex(String text) {
-        return text.matches(PATTERN.pattern());
+        matcher = PATTERN.matcher(text);
+        return matcher.matches();
     }
 }
