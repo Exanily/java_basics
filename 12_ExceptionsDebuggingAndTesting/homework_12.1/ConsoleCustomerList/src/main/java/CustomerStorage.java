814de3cd874b375e2962076e679fd4a6223ff6d8
BuildTools <unconfigured@null.spigotmc.org>
2021-11-01 19:21:45 +0300

    task done

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,7 +1,14 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
+    public static final Pattern PATTERN_MAIL = Pattern.compile("[\\w.-]+@[A-z]+\\.[A-z]+");
+    public static final Pattern PATTERN_PHONE = Pattern.compile("\\+[0-9]+");
+    private static final String ADD_COMMAND = "add Василий Петров " +
+            "vasily.petrov@gmail.com +79215637722";
+    private static final String COMMAND_ERROR = "Wrong command! Available command examples: \n\t" +
+            ADD_COMMAND;
     private final Map<String, Customer> storage;
 
     public CustomerStorage() {
@@ -13,8 +20,16 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
-
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new ArrayIndexOutOfBoundsException(COMMAND_ERROR);
+        }
+        if (!PATTERN_MAIL.matcher(components[INDEX_EMAIL]).matches()) {
+            throw new ArrayIndexOutOfBoundsException("Wrong mail!");
+        }
+        if (!PATTERN_PHONE.matcher(components[INDEX_PHONE]).matches()) {
+            throw new ArrayIndexOutOfBoundsException("Wrong phone!");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
