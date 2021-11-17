eedb6adb927b41ffd1123da283cd8c0592d3d70c
BuildTools <unconfigured@null.spigotmc.org>
2021-11-17 20:41:50 +0300

    task done

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long byte1 = 0;
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            byte1 = paths
+                    .filter(Files::isRegularFile).mapToLong(path1 -> path1.toFile().length()).sum();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return byte1;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,45 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        for (; ; ) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                String line = scanner.nextLine().trim();
+                print(line);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private static void print(String line) {
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        final double const1 = 1024;
+        long byte1 = FileUtils.calculateFolderSize(line);
+        double KByte = byte1 / const1;
+        double result;
+        String dimension;
+        if (KByte >= 1) {
+            double MByte = KByte / const1;
+            if (MByte >= 1) {
+                double GByte = MByte / const1;
+                if (GByte >= 1) {
+                    result = GByte;
+                    dimension = "ГБайт";
+                } else {
+                    result = MByte;
+                    dimension = "МБайт";
+                }
+            } else {
+                result = KByte;
+                dimension = "KБайт";
+            }
+        } else {
+            result = byte1;
+            dimension = "байт";
+        }
+        System.out.println(formatter.format(result) + " " + dimension);
     }
 }
