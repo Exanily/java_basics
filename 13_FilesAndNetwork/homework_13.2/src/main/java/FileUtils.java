fb3a5d2fa52c544e456172158c4525a71ee0626e
BuildTools <unconfigured@null.spigotmc.org>
2021-11-22 10:42:16 +0300

    task done

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        try {
+            File sourceDir = new File(sourceDirectory);
+            File destDir = new File(destinationDirectory);
+            Path destPath = destDir.toPath();
+            Path sourcePath = sourceDir.toPath();
+            try (Stream<Path> paths = Files.walk(Paths.get(sourceDirectory))) {
+                paths.forEach(path -> {
+                    System.out.println(destPath.resolve(sourcePath.relativize(path)));
+                    Path dest = destPath.resolve(sourcePath.relativize(path));
+                    if (Files.exists(dest)) {
+                        try {
+                            Files.delete(dest);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    try {
+                        Files.copy(path, dest);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //for (; ; ) {
+            try {
+                System.out.println("Введите путь к папке которую нужно скопировать : ");
+                Scanner scanner = new Scanner(System.in);
+                String sourceDirectory = scanner.nextLine().trim();
+                System.out.println("Введите путь к папке которую нужно скопировать : ");
+                scanner = new Scanner(System.in);
+                String destinationDirectory = scanner.nextLine().trim();
+                FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        //}
     }
 }
