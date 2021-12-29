39c440c6585aeef1595b576d85a0041e3f9e1c1e
BuildTools <unconfigured@null.spigotmc.org>
2021-12-30 00:30:32 +0300

    task done

--- 13_FilesAndNetwork/ParseHTML/src/Main.java
+++ 13_FilesAndNetwork/ParseHTML/src/Main.java
@@ -1,5 +1,42 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Main {
     public static void main(String[] args) {
+        String filename = "images";
+        Path pathToFile = Paths.get(filename);
+        if (!new File(pathToFile.toString()).exists()) {
+            File file = new File(filename);
+            file.mkdir();
+        }
+        try {
+            Document document = Jsoup.connect("https://lenta.ru").get();
+            Elements elements = document.select("div.layout__container").select("img");
 
+            elements.forEach(element -> {
+                System.out.println(element.attr("src"));
+                int i = element.attr("src").lastIndexOf("/");
+                String name = element.attr("src").substring(i);
+                try (InputStream in = new URL(element.attr("src")).openStream()) {
+                    String s = pathToFile.toAbsolutePath() + name;
+                    if (!new File(s).exists()) {
+                        Files.copy(in, Path.of(s));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
+
 }
