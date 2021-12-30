9d7b4be1e135ffb0b007e70e54093557860a14e1
BuildTools <unconfigured@null.spigotmc.org>
2021-12-31 00:05:33 +0300

    added check

--- 13_FilesAndNetwork/ParseHTML/src/Main.java
+++ 13_FilesAndNetwork/ParseHTML/src/Main.java
@@ -7,15 +7,24 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(".jpg");
+        arrayList.add(".jpeg");
+        arrayList.add(".png");
+
         String filename = "images";
         Path pathToFile = Paths.get(filename);
         if (!new File(pathToFile.toString()).exists()) {
             File file = new File(filename);
-            if(file.mkdir()){
+            if (file.mkdir()) {
                 System.out.println("Файл " + filename + " создан");
             }
         }
@@ -24,16 +33,18 @@ public class Main {
             Elements elements = document.select("div.layout__container").select("img");
 
             elements.forEach(element -> {
-                System.out.println(element.attr("src"));
                 int i = element.attr("src").lastIndexOf("/");
                 String name = element.attr("src").substring(i);
-                try (InputStream in = new URL(element.attr("src")).openStream()) {
-                    String s = pathToFile.toAbsolutePath() + name;
-                    if (!new File(s).exists()) {
-                        Files.copy(in, Path.of(s));
+                if (arrayList.contains(name.substring(name.lastIndexOf(".")))) {
+                    try (InputStream in = new URL(element.attr("src")).openStream()) {
+                        String s = pathToFile.toAbsolutePath() + name;
+                        if (!new File(s).exists()) {
+                            Files.copy(in, Path.of(s));
+                            System.out.println(element.attr("src"));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             });
         } catch (Exception ex) {
