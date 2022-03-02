5a924f2e46498318212d49b2ccdc0cad23fb1880
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 12:55:53 +0300

    task done

--- SpringBootWebDevelopment/ToDoList/src/main/java/main/Storage.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/Storage.java
@@ -3,11 +3,12 @@ package main;
 import response.ToDoList;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Storage {
     private static int currentId = 1;
-    private static final HashMap<Integer, ToDoList> toDoLists = new HashMap<>();
+    private static ConcurrentHashMap<Integer, ToDoList> toDoLists = new ConcurrentHashMap<>();
 
     public static int addToDo(ToDoList list) {
         int id = currentId++;
@@ -20,9 +21,42 @@ public class Storage {
         return new ArrayList<>(toDoLists.values());
     }
 
-   public static ToDoList getToDo(int id) {
-        if (toDoLists.containsKey(id)){
+    public static ToDoList getToDo(int id) {
+        if (toDoLists.containsKey(id)) {
             return toDoLists.get(id);
+        }
+        return null;
+    }
+
+    public static void deleteAll() {
+        toDoLists = new ConcurrentHashMap<>();
+    }
+
+    public static Integer delete(int id) {
+        if (toDoLists.containsKey(id)) {
+            toDoLists.remove(id);
+            return id;
+        }
+        return null;
+    }
+
+    public static List<Integer> putAll(ArrayList<ToDoList> lists) {
+        ArrayList<Integer> ids = new ArrayList<>();
+        lists.forEach(list -> {
+            int id = list.getId();
+            if (toDoLists.containsKey(id)) {
+
+                toDoLists.replace(id, list);
+                ids.add(id);
+            }
+        });
+        return ids;
+    }
+
+    public static Integer put(int id, ToDoList list) {
+        if (toDoLists.containsKey(id)) {
+            toDoLists.replace(id, list);
+            return id;
         }
         return null;
     }
--- SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoListController.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoListController.java
@@ -5,27 +5,52 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import response.ToDoList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ToDoListController {
-    @GetMapping("/ToDo/")
+    @GetMapping("/todo/")
     public List<ToDoList> list() {
         return Storage.getAllToDo();
     }
 
-    @RequestMapping(value = "/Todo/", method = RequestMethod.POST)
+    @PostMapping("/todo/")
     public int add(ToDoList list) {
         return Storage.addToDo(list);
     }
 
-    @GetMapping("/Todo/{id}")
+    @GetMapping("/todo/{id}")
     public ResponseEntity get(@PathVariable int id) {
         ToDoList list = Storage.getToDo(id);
         if (list == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/todo/")
+    public String deleteAll() {
+        Storage.deleteAll();
+        return "Все дела удалены";
+    }
+
+    @DeleteMapping("/todo/{id}")
+    public ResponseEntity deleteId(@PathVariable int id) {
+        Integer integer = Storage.delete(id);
+        if (integer == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity(integer, HttpStatus.OK);
+    }
+
+    @PutMapping("/todo/{id}")
+    public ResponseEntity put(@PathVariable int id, ToDoList list) {
+        Integer integer = Storage.put(id, list);
+        if (integer == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity(integer, HttpStatus.OK);
     }
 
 }
